515e781c9bcb9e386e07654146e7eb3566d4a316
9gl <glunkad007@gmail.com>
2022-08-19 16:52:37 +0530

    Simple React project

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img className="nav--icon" src="https://i0.wp.com/programmingwithmosh.com/wp-content/uploads/2019/01/2000px-React-icon.svg_.png?fit=2000%2C1413&ssl=1" alt="React"/>
+            <h3 className="nav--logo_text" >RealFacts</h3>
+            <h4 className="nav--title" >React Course - Project 1</h4>
+        </nav>
+    )
+}
